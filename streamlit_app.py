7b15e64f1977de2467650b61532d5fe205630c09
Ruturajgothwal1999 <ruturajrajput3@gmail.com>
2024-09-20 15:16:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,11 @@ my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_N
 ingredients_list=st.multiselect('Choose upto 5 ingredients: ',my_dataframe,max_selections=5)
 if ingredients_list:
     ingredients_string=''
+    
     for fruit_chosen in ingredients_list:
-        ingredients_string +=fruit_chosen + ''
+            ingredients_string +=fruit_chosen + ' '
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -29,10 +32,4 @@ if ingredients_list:
     time_to_insert=st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")     
-     
-#New session to display fruityyvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
